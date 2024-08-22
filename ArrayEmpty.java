4032e2eaa80f67591489274d647510f2763b502d
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2024-08-22 23:05:49 +0530

    Add files via upload

---
+++ ArrayEmpty.java
@@ -0,0 +1,18 @@
+// Minimum Number of Operations to Make Array Empty
+class ArrayEmpty {
+    public int minOperations(int[] nums) {
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        }
+        int ans=0;
+        for(int it: hm.keySet()){
+            int freqOfKey=hm.get(it);
+            if (freqOfKey == 1) {
+                return -1;
+            }
+            ans+=Math.ceil((double)freqOfKey/3);
+        }
+        return ans;       
+    }
+}
