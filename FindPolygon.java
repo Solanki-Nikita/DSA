8ed16a9ddd35efdbc175a78d58d8315cbac9f15e
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2024-09-19 19:32:40 +0530

    Add files via upload

---
+++ FindPolygon.java
@@ -0,0 +1,20 @@
+// Find Polygon With the Largest Perimeter
+
+class FindPolygon {
+    public long largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        
+        long previous_elements_sum = 0;
+        long ans = -1;
+
+        for (int num : nums) {
+            if (num < previous_elements_sum) {
+                ans = Math.max(ans, previous_elements_sum + num);
+            }
+
+            previous_elements_sum += num;
+        }
+
+        return ans;
+    }
+}
