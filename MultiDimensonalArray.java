bca76f1efefb88fd04a95d21d42fcd10e54f61d3
Solanki-Nikita <90173555+Solanki-Nikita@users.noreply.github.com>
2024-10-20 22:45:27 +0530

    Add files via upload

---
+++ MultiDimensonalArray.java
@@ -0,0 +1,29 @@
+// MultiDimensonalArray
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MultiDimensonalArray{
+	public static void main(String[] args){
+	
+	Scanner in = new Scanner(System.in);
+	ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+	//initializing 
+	for(int i=0; i<3 ;i++){
+		list.add(new ArrayList<>());
+	}
+
+	//to add element
+	for(int i=0 ;i<3; i++){
+	    for(int j=0 ; j<3 ; j++){
+		list.get(i).add(in.nextInt());
+	    }
+	}
+
+	// print arraylist
+		System.out.println(list);
+
+
+	}
+}
